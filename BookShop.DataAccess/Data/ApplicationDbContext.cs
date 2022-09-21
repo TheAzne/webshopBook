ba7e92c064ad0277eafa162d75bc1e7fe1dbb7c0
azne <a_kurbanov@hotmail.com>
2022-09-21 23:10:02 +0200

    initial commit

---
+++ BookShop.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using BookShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.DataAccess;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
---
+++ BookShop.Utility/SD.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+
+
+namespace BookShop.Utility;
+
+public static class SD
+{
+
+}
