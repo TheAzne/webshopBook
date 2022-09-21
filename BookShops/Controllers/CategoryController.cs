4a5e858a224d64725059e88bf18b163af1ecfa66
azne <a_kurbanov@hotmail.com>
2022-09-22 00:35:06 +0200

    update

---
+++ BookShops/Controllers/CategoryController.cs
@@ -0,0 +1,129 @@
+using BookShop.DataAccess;
+using BookShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Controllers;
+
+public class CategoryController : Controller
+{
+
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+
+    public IActionResult Index()
+    {
+        IEnumerable<Category> categoryList = _db.Categories;
+        return View(categoryList);
+    }
+
+    //GET
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The display order cannot match the name");
+        }
+
+
+
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+
+    }
+
+
+    //GET
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var categoryfromDb = _db.Categories.Find(id);
+
+        if (categoryfromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryfromDb);
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The display order cannot match the name");
+        }
+
+
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+
+    }
+
+
+    //GET
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var categoryfromDb = _db.Categories.Find(id);
+
+        if (categoryfromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryfromDb);
+    }
+
+    //POST
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePost(int? id)
+    {
+        var categoryfromDb = _db.Categories.Find(id);
+
+        if (categoryfromDb == null)
+        {
+            return NotFound();
+        }
+
+        _db.Categories.Remove(categoryfromDb);
+        _db.SaveChanges();
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
+
+    }
+
+
+}
