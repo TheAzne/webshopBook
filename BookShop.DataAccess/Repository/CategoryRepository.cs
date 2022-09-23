d3ea06d118fef7f5600bf4062b2a0157b7939e49
azne <a_kurbanov@hotmail.com>
2022-09-23 10:13:16 +0200

    Created CategoryRepository

---
+++ BookShop.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookShop.DataAccess.Repository.IRepository;
+using BookShop.Models;
+
+namespace BookShop.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Update(obj);
+        }
+    }
+}
