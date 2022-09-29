7071de30fa4887f186d9fad6b7b3861400be4321
azne <a_kurbanov@hotmail.com>
2022-09-29 15:29:38 +0200

    Created detail page

--- BookShops/Controllers/HomeController.cs
+++ BookShops/Controllers/HomeController.cs
@@ -1,21 +1,45 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BookShop.Models;
-
+using BookShop.Models.ViewModels;
+using BookShop.DataAccess;
+using BookShop.DataAccess.Repository.IRepository;
+using BookShop.Utility;
+using BookShop.Models;
+using BookShop.Models.ViewModels;
 namespace BookShops.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> productList = _unitOfWork.Product.GetAll(
+            includeProperties: "Category,CoverType"
+        );
+
+        return View(productList);
+    }
+
+    public IActionResult Details(int id)
+    {
+        ShoppingCart cartObj = new()
+        {
+            Count = 1,
+            Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType")
+
+        };
+
+    return View(cartObj);
+
     }
 
     public IActionResult Privacy()
